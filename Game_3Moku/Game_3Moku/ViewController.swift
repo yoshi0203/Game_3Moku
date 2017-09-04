575abf0fe75c1051a9f0a80cf8d1c3265a99ba06
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2017-09-04 23:14:35 +0900

    xCode環境作成

---
+++ Game_3Moku/Game_3Moku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game_3Moku
+//
+//  Created by 山中力仁 on 2017/09/04.
+//  Copyright © 2017年 yamayoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
