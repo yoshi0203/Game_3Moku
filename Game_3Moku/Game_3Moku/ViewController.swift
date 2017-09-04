41d4363cf74252ad2f65fbf4b92e205428866357
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2017-09-05 01:12:44 +0900

    ボタンを追加

--- Game_3Moku/Game_3Moku/ViewController.swift
+++ Game_3Moku/Game_3Moku/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func evButton5(_ sender: UIButton) {
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
