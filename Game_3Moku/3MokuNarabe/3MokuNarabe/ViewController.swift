bb61776533bbf11758f6915b02b563770ddd74c0
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2017-09-20 20:13:25 +0900

    コンパイルエラー原因わからず、再作成。 前の文は一応残しとく。

---
+++ Game_3Moku/3MokuNarabe/3MokuNarabe/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  3MokuNarabe
+//
+//  Created by 山中力仁 on 2017/09/20.
+//  Copyright © 2017年 yamayoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
+    @IBOutlet weak var Button7: UIButton!
+    @IBOutlet weak var Button8: UIButton!
+    @IBOutlet weak var Button9: UIButton!
+    
+    @IBAction func evButton1(_ sender: UIButton) {
+    }
+    @IBAction func evButton2(_ sender: UIButton) {
+    }
+    @IBAction func evButton3(_ sender: UIButton) {
+    }
+    @IBAction func evButton4(_ sender: UIButton) {
+    }
+    @IBAction func evButton5(_ sender: UIButton) {
+//        Button5.setImage(UIImage(named: "maru.PNG"), for: UIControlState())
+        Button5.setTitle("○", for: .normal)
+    }
+    @IBAction func evButton6(_ sender: UIButton) {
+    }
+    @IBAction func evButton7(_ sender: UIButton) {
+    }
+    @IBAction func evButton8(_ sender: UIButton) {
+    }
+    @IBAction func evButton9(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
