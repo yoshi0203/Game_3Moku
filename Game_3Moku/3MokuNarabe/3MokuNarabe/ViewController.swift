72d58c406315da89b9ff62079e4c31fcb27c36fd
yoshitoyamanka <yoshito.y@Yoshitos-MacBook-Pro.local>
2017-09-20 23:30:49 +0900

    押した回数のカウンタを追加

--- Game_3Moku/3MokuNarabe/3MokuNarabe/ViewController.swift
+++ Game_3Moku/3MokuNarabe/3MokuNarabe/ViewController.swift
@@ -20,25 +20,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button8: UIButton!
     @IBOutlet weak var Button9: UIButton!
     
+    var cnt:Int = 1
+    var sign:String = "○"
+    
     @IBAction func evButton1(_ sender: UIButton) {
+        //ここをメソッド化したいね
+        if cnt % 2 == 0 {
+            sign = "×"
+        }else{
+            sign = "○"
+        }
+        Button1.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton2(_ sender: UIButton) {
+        Button2.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton3(_ sender: UIButton) {
+        Button3.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton4(_ sender: UIButton) {
+        Button4.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton5(_ sender: UIButton) {
-//        Button5.setImage(UIImage(named: "maru.PNG"), for: UIControlState())
-        Button5.setTitle("○", for: .normal)
+        Button5.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton6(_ sender: UIButton) {
+        Button6.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton7(_ sender: UIButton) {
+        Button7.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton8(_ sender: UIButton) {
+        Button8.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     @IBAction func evButton9(_ sender: UIButton) {
+        Button9.setTitle(sign , for: .normal)
+        cnt = cnt + 1
     }
     
     override func viewDidLoad() {
